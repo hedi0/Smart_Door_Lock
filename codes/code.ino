63d1b47ff7326df47c564e6fa24ca939ceb82983
Med Hedi Abd <102860383+hedi0@users.noreply.github.com>
2026-02-19 17:36:15 +0100

    Update code.ino

--- codes/code.ino
+++ codes/code.ino
@@ -41,5 +41,8 @@ void loop() {
 int getFingerprintID() {
   uint8_t p = finger.getImage();
   if (p != FINGERPRINT_OK) return -1;
+p = finger.image2Tz();
+  if (p != FINGERPRINT_OK) return -1;
 
+  
   
